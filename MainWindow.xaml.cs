94d50c81bce5dea8ead7cb0b521b8f7603651c2f
Antoine <72378754+Antoinethl85@users.noreply.github.com>
2021-10-03 22:30:30 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,18 +27,94 @@ namespace WpfApp1
             InitializeComponent();
         }
         
-        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        private void ButtonDecode_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(inputText.Text) && !outputText.Text.Contains(inputText.Text))
             {
-                //outputText.Text.Add(inputText.Text);
+                outputText.Text = Decodage.Decode(inputText.Text, inputKey.Text, inputVector.Text);
                 inputText.Clear();
+                inputKey.Clear();
+                inputVector.Clear();
+            }
+        }
+        
+        private void ButtonEncode_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(inputTextC.Text) && !outputTextC.Text.Contains(inputTextC.Text))
+            {
+                outputTextC.Text = Encodage.Encode(inputTextC.Text, inputKeyC.Text, inputVectorC.Text);
+                inputTextC.Clear();
+                inputKeyC.Clear();
+                inputVectorC.Clear();
             }
         }
     }
 
-    internal static class decodage
+    internal static class Decodage
     {
-        
+        public static string Decode(string inputText, string inputKey, string inputVect)
+        {
+            try
+            {
+                byte[] key = Encoding.UTF8.GetBytes(inputKey);
+                byte[] vect = Encoding.UTF8.GetBytes(inputVect);
+            
+                RijndaelManaged rijndael = new RijndaelManaged();
+            
+                ICryptoTransform decryptor = rijndael.CreateDecryptor(key, vect);
+                
+                MemoryStream msDecrypt = new MemoryStream();
+                CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write);
+                StreamWriter swDecrypt = new StreamWriter(csDecrypt);
+                
+                swDecrypt.Write(inputText);
+                
+                byte[] decodeText = msDecrypt.ToArray();
+                
+                msDecrypt.Close();
+                csDecrypt.Close();
+                swDecrypt.Close();
+            
+                return Convert.ToBase64String(decodeText);
+            }
+            catch (Exception e)
+            {
+                return "Decryption failed";
+            }
+        }
+    }
+
+    internal static class Encodage
+    {
+        public static string Encode(string inputText, string inputKey, string inputVect)
+        {
+            try
+            {
+                byte[] key = Encoding.UTF8.GetBytes(inputKey);
+                byte[] vect = Encoding.UTF8.GetBytes(inputVect);
+            
+                RijndaelManaged rijndael = new RijndaelManaged();
+            
+                ICryptoTransform encryptor = rijndael.CreateEncryptor(key, vect);
+                
+                MemoryStream msEncrypt = new MemoryStream();
+                CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+                StreamWriter swEncrypt = new StreamWriter(csEncrypt);
+                
+                swEncrypt.Write(inputText);
+                
+                byte[] encodeText = msEncrypt.ToArray();
+                
+                msEncrypt.Close();
+                csEncrypt.Close();
+                swEncrypt.Close();
+            
+                return Convert.ToBase64String(encodeText);
+            }
+            catch (Exception e)
+            {
+                return "Encryption failed";
+            }
+        }
     }
 }
